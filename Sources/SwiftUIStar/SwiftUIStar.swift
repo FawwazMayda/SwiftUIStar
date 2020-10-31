905fedde96c1cdcad31aa62bb828d9481b0eb723
maydafawwaz53 <mayda.fawwaz53@gmail.com>
2020-10-31 09:25:06 +0800

    add star count state

--- Sources/SwiftUIStar/SwiftUIStar.swift
+++ Sources/SwiftUIStar/SwiftUIStar.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 
 struct StarView: View {
+    @State var selectedIndex = -1
     var starCount = 5
     var body: some View {
         HStack {
             ForEach(0..<starCount){i in
-                Image("unfilled", bundle: Bundle.module)
+                Image((i <= selectedIndex) ? "filled" : "unfilled",bundle: Bundle.module).onTapGesture(count: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/, perform: {
+                    self.selectedIndex = i
+                })
             }
         }
     }
